3bbb27cf7dd6cd5f6e25e7cc7938c5e4bb881457
orientye <orientye@qq.com>
2025-02-01 20:44:01 +0800

    orient-dotnet

--- CoreRPC/Util/NetworkHelper.cs
+++ CoreRPC/Util/NetworkHelper.cs
@@ -1,23 +1,21 @@
-﻿using System.Net.Sockets;
-using System.Net;
+﻿using System.Net;
+using System.Net.Sockets;
 
-namespace CoreRPC.Util
+namespace CoreRPC.Util;
+
+public static class NetworkHelper
 {
-    public class NetworkHelper
+    public static string GetLocalIPv4()
     {
-        public static string GetLocalIPv4()
-        {
-            string ipstr = "127.0.0.1";
-            var host = Dns.GetHostEntry(Dns.GetHostName());
-            foreach (var ip in host.AddressList)
+        var ipStr = "127.0.0.1";
+        var host = Dns.GetHostEntry(Dns.GetHostName());
+        foreach (var ip in host.AddressList)
+            if (ip.AddressFamily == AddressFamily.InterNetwork)
             {
-                if (ip.AddressFamily == AddressFamily.InterNetwork)
-                {
-                    ipstr = ip.ToString();
-                    break;
-                }
+                ipStr = ip.ToString();
+                break;
             }
-            return ipstr;
-        }
+
+        return ipStr;
     }
 }
