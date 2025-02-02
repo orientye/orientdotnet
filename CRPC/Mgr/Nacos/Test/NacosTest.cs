ee67497de21fcd899e892754e8f9432a814d953a
orientye <orientye@qq.com>
2025-02-02 12:15:18 +0800

    orient-dotnet

--- CRPC/Mgr/Nacos/Test/NacosTest.cs
+++ CRPC/Mgr/Nacos/Test/NacosTest.cs
@@ -1,33 +1,40 @@
-﻿using Nacos.V2.Common;
+﻿using CRPC.Util;
+using Nacos.V2.Common;
 
-namespace CRPC.Mgr.Nacos.Test
+namespace CRPC.Mgr.Nacos.Test;
+
+public class NacosTest
 {
-    public class NacosTest
+    public static async Task Test()
     {
-        public static async Task Test()
-        {
-            #region nacos init
-            NacosMgr.Instance.Init("http://127.0.0.1:8848/", "945cd984-88e8-438c-8bc6-d0d5ccbfefa0", "nacos", "nacos");
-            #endregion
+        #region nacos init
 
-            #region regiser service
-            string lanip = Util.NetworkHelper.GetLocalIPv4();
-            string dt = DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss.fff");
-            await NacosMgr.Instance.Registry.RegisterInstance(
-                "egotao:fight-srv",
-                Constants.DEFAULT_GROUP,
-                $"{lanip}",
-                7999, 
-                new Dictionary<string, string> {
-                    { "srvId", "1002" }, 
-                    { "startTime", $"{dt}" },
-                    { "lanIp", $"{lanip}" }
-                });
-            #endregion
+        NacosMgr.Instance.Init("http://127.0.0.1:8848/", "945cd984-88e8-438c-8bc6-d0d5ccbfefa0", "nacos", "nacos");
 
-            #region test
-            await CRPC.Test.DoTest();
-            #endregion
-        }
+        #endregion
+
+        #region regiser service
+
+        var localIPv4 = NetworkHelper.GetLocalIPv4();
+        var dt = DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss.fff");
+        await NacosMgr.Instance.Registry.RegisterInstance(
+            "test-service",
+            Constants.DEFAULT_GROUP,
+            $"{localIPv4}",
+            7999,
+            new Dictionary<string, string>
+            {
+                { "srvId", "1001" },
+                { "startTime", $"{dt}" },
+                { "lanIp", $"{localIPv4}" }
+            });
+
+        #endregion
+
+        #region test
+
+        await CRPC.Test.DoTest();
+
+        #endregion
     }
 }
