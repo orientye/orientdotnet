ee54e0bea2eb0bfacdf3700d2b8909de0badf8be
yeds <yeds@mail.jj.cn>
2026-04-29 21:56:32 +0800

    thread

--- Tests/CRPC.Tests/CRpcClientTests.cs
+++ Tests/CRPC.Tests/CRpcClientTests.cs
@@ -9,12 +9,18 @@ public class CRpcClientTests
     [Fact]
     public void CallAsyncThrowsWhenNoCRpcLoopIsBound()
     {
-        var client = new CRpcClient();
+        Exception? exception = null;
+        var worker = new Thread(() =>
+        {
+            var client = new CRpcClient();
+            exception = Assert.Throws<InvalidOperationException>(() =>
+                client.CallAsync(1, 1, Array.Empty<byte>(), timeout: 0));
+        });
 
-        var exception = Assert.Throws<InvalidOperationException>(() =>
-            client.CallAsync(1, 1, Array.Empty<byte>(), timeout: 0));
+        worker.Start();
+        worker.Join();
 
-        Assert.Contains("CRpcLoop", exception.Message);
+        Assert.Contains("CRpcLoop", exception!.Message);
     }
 
     [Fact]
@@ -58,6 +64,43 @@ public class CRpcClientTests
     }
 
     [Fact]
+    public void CallAsyncMatchesOutOfOrderResponsesToPendingCalls()
+    {
+        var loop = new CRpcLoop();
+        loop.BindToCurrentThread();
+
+        var client = new CRpcClient();
+        var firstTask = client.CallAsync(7, 8, Array.Empty<byte>(), timeout: 0);
+        var secondTask = client.CallAsync(7, 8, Array.Empty<byte>(), timeout: 0);
+        var firstAwaiter = firstTask.GetAwaiter();
+        var secondAwaiter = secondTask.GetAwaiter();
+
+        CRpcMessage? firstResult = null;
+        CRpcMessage? secondResult = null;
+        firstAwaiter.OnCompleted(() => firstResult = firstAwaiter.GetResult());
+        secondAwaiter.OnCompleted(() => secondResult = secondAwaiter.GetResult());
+
+        var firstResponse = CreateResponse(reqSequence: 1);
+        var secondResponse = CreateResponse(reqSequence: 2);
+
+        var worker = new Thread(() =>
+        {
+            client.OnReceiveResponse(secondResponse);
+            client.OnReceiveResponse(firstResponse);
+        });
+        worker.Start();
+        worker.Join();
+
+        Assert.False(firstAwaiter.IsCompleted);
+        Assert.False(secondAwaiter.IsCompleted);
+
+        loop.Tick();
+
+        Assert.Same(firstResponse, firstResult);
+        Assert.Same(secondResponse, secondResult);
+    }
+
+    [Fact]
     public void CallAsyncTimeoutCompletesOnlyWhenCallingLoopTicks()
     {
         var loop = new CRpcLoop();
@@ -75,5 +118,16 @@ public class CRpcClientTests
 
         Assert.True(awaiter.IsCompleted);
         Assert.Throws<TimeoutException>(() => awaiter.GetResult());
+    }
+
+    private static CRpcMessage CreateResponse(long reqSequence)
+    {
+        var responseHeader = CRpcMessageHeader.valueOf(
+            CRpcMessageState.STATE_RESPONSE,
+            resultCode: 0,
+            sn: reqSequence,
+            module: 7,
+            command: 8);
+        return CRpcMessage.valueOf(responseHeader, Array.Empty<byte>());
     }
 }
