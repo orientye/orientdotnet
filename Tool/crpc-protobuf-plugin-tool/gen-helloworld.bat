b33110980515aae13ae96d00ebdfa804c94d2694
orientye <orientye@qq.com>
2026-06-19 16:57:59 +0800

    tool

--- Tool/crpc-protobuf-plugin-tool/gen-helloworld.bat
+++ Tool/crpc-protobuf-plugin-tool/gen-helloworld.bat
@@ -1,29 +1,59 @@
 @echo off
-setlocal
+setlocal EnableDelayedExpansion
+
+set ERR=0
 
 set PROTOBUF_TOOLS=%userprofile%\.nuget\packages\google.protobuf.tools\3.35.0-rc1
 set PROTOC=%PROTOBUF_TOOLS%\tools\windows_x64\protoc.exe
 set PROTO_INCLUDE=%PROTOBUF_TOOLS%\tools\include
 
 if not exist "%PROTOC%" (
-  echo protoc not found: %PROTOC%
+  echo ERROR: protoc not found: %PROTOC%
   echo Restore Google.Protobuf.Tools first:
   echo   dotnet restore ..\crpc-protobuf-plugin\CRpcProtobufPlugin\CRpcProtobufPlugin.csproj
-  exit /b 1
+  set ERR=1
+  goto :finish
 )
 
-"%PROTOC%" --version
+set "STEP=protoc --version"
+set "STEP_ERR=protoc --version failed."
+call :RunProtoc --version
 
-"%PROTOC%" --csharp_out=../../Example/HelloWorld --proto_path=../../Example/HelloWorld helloworld-msg.proto
-if errorlevel 1 exit /b 1
+set "STEP=Generating helloworld-msg.cs ..."
+set "STEP_ERR=failed to generate message code from helloworld-msg.proto"
+call :RunProtoc --csharp_out=../../Example/HelloWorld --proto_path=../../Example/HelloWorld helloworld-msg.proto
 
-"%PROTOC%" --plugin=protoc-gen-crpc.exe --crpc_out=../../Example/HelloWorld --proto_path=%PROTO_INCLUDE% --proto_path=../../CRpc/Rpc/CRpc/Protobuf --proto_path=../../Example/HelloWorld helloworld.proto
-if errorlevel 1 exit /b 1
+set "STEP=Generating HelloworldService.cs / HelloworldClient.cs ..."
+set "STEP_ERR=failed to generate CRpc code from helloworld.proto"
+set "STEP_HINT=Check protoc-gen-crpc.exe and protoc-gen-crpc.dll in this folder."
+call :RunProtoc --plugin=protoc-gen-crpc.exe --crpc_out=../../Example/HelloWorld --proto_path=%PROTO_INCLUDE% --proto_path=../../CRpc/Rpc/CRpc/Protobuf --proto_path=../../Example/HelloWorld helloworld.proto
 
 copy /Y ..\..\Example\HelloWorld\HelloworldMsg.cs ..\..\Example\HelloWorld\Server\
 move /Y ..\..\Example\HelloWorld\HelloworldService.cs ..\..\Example\HelloWorld\Server\
 move /Y ..\..\Example\HelloWorld\HelloworldMsg.cs ..\..\Example\HelloWorld\Client\
 move /Y ..\..\Example\HelloWorld\HelloworldClient.cs ..\..\Example\HelloWorld\Client\
 
+echo.
 echo Done.
+goto :finish
+
+:RunProtoc
+echo.
+echo %STEP%
+"%PROTOC%" %*
+if errorlevel 1 (
+  echo ERROR: %STEP_ERR%
+  if defined STEP_HINT echo %STEP_HINT%
+  set ERR=1
+  goto :finish
+)
+set "STEP_HINT="
+exit /b 0
+
+:finish
+if %ERR%==1 (
+  echo.
+  echo gen-helloworld failed.
+)
 pause
+exit /b %ERR%
